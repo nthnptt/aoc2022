3ac0ce6c62890910dbcadcae30daea33f134e720
nthnptt <yaman0@protonmail.com>
2022-11-30 20:11:05 +0100

    :tada:

---
+++ src/index.spec.ts
@@ -0,0 +1,8 @@
+describe('Testint test', () => {
+    it('should works', () => {
+        expect(true).toBeTruthy();
+    })
+    it('should not works', () => {
+        expect(false).toBeTruthy();
+    })
+})
